461b498536d4a0eab50439677861b5a0b4aa373d
Israel <israel.nicacio@outlook.com>
2020-03-05 12:36:44 -0300

    implementando canActive

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-  canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
-    throw new Error("Method not implemented.");
-  }
 
   constructor() { }
+
+  canActivate(): boolean {
+    return false;
+  }
 }
